23b0714afc6b2a5264db3227f959f5ab93946f4a
abhishek verma <abhishek.verma@cybrilla.com>
2018-06-25 20:43:51 +0530

    Fix tooltio fadeout on text hover

--- src/bubbles.js
+++ src/bubbles.js
@@ -1,6 +1,6 @@
-import React from "react";
-import * as d3 from "d3";
-import tooltip from "./tooltip";
+import React from 'react';
+import * as d3 from 'd3';
+import tooltip from './tooltip';
 
 export default class Bubbles extends React.Component {
   constructor(props) {
@@ -10,21 +10,21 @@ export default class Bubbles extends React.Component {
       .forceSimulation()
       .velocityDecay(0.2)
       .force(
-        "x",
+        'x',
         d3
           .forceX()
           .strength(forceStrength)
           .x(center.x)
       )
       .force(
-        "y",
+        'y',
         d3
           .forceY()
           .strength(forceStrength)
           .y(center.y)
       )
-      .force("charge", d3.forceManyBody().strength(this.charge.bind(this)))
-      .on("tick", this.ticked.bind(this))
+      .force('charge', d3.forceManyBody().strength(this.charge.bind(this)))
+      .on('tick', this.ticked.bind(this))
       .stop();
   }
 
@@ -42,12 +42,12 @@ export default class Bubbles extends React.Component {
 
   ticked = () => {
     this.state.g
-      .selectAll(".bubble")
-      .attr("cx", d => d.x)
-      .attr("cy", d => d.y);
+      .selectAll('.bubble')
+      .attr('cx', d => d.x)
+      .attr('cy', d => d.y);
     this.state.g
-      .selectAll(".label")
-      .attr("transform", d => `translate(${d.x}, ${d.y})`);
+      .selectAll('.label')
+      .attr('transform', d => `translate(${d.x}, ${d.y})`);
   };
 
   charge = d => {
@@ -60,7 +60,7 @@ export default class Bubbles extends React.Component {
   };
 
   renderBubbles = data => {
-    const bubbles = this.state.g.selectAll(".bubble").data(data, d => d.id);
+    const bubbles = this.state.g.selectAll('.bubble').data(data, d => d.id);
 
     // Exit
     bubbles.exit().remove();
@@ -68,33 +68,35 @@ export default class Bubbles extends React.Component {
     // Enter
     const bubblesE = bubbles
       .enter()
-      .append("circle")
-      .classed("bubble", true)
-      .attr("r", 0)
-      .attr("cx", d => d.x)
-      .attr("cy", d => d.y)
-      .attr("fill", d => d.color)
+      .append('circle')
+      .classed('bubble', true)
+      .attr('r', 0)
+      .attr('cx', d => d.x)
+      .attr('cy', d => d.y)
+      .attr('fill', d => d.color)
       // .attr('fill', d => fillColor(d.group))
       // .attr('stroke', d => d3.rgb(fillColor(d.group)).darker())
       // .attr("stroke-width", 2)
-      .on("mouseover", showDetail)
-      .on("mouseout", hideDetail);
+      .on('mouseover', showDetail)
+      .on('mouseout', hideDetail);
     const labels = bubbles
       .enter()
-      .append("text")
-      .attr("text-anchor", "middle")
-      .attr("fill", "white")
-      .classed("label", true)
+      .append('text')
+      .attr('text-anchor', 'middle')
+      .attr('fill', 'white')
+      .classed('label', true)
       .html(function(d) {
         return `<tspan x=0 y=-20 dy="1em">${d.radius}</tspan>
                 <tspan x=0 y=-4 dy="1.2em">${d.name}</tspan>`;
-      });
+      })
+      .on('mouseover', showDetailLabelHover)
+      .on('mouseout', hideDetailLabelHover);
     labels.transition().duration(0);
     bubblesE
       .transition()
       .duration(0)
-      .attr("r", d => d.radius)
-      .on("end", () => {
+      .attr('r', d => d.radius)
+      .on('end', () => {
         this.simulation
           .nodes(data)
           .alpha(1)
@@ -112,23 +114,21 @@ export default class Bubbles extends React.Component {
 * details of a bubble in the tooltip.
 */
 export function showDetail(d) {
-  // change outline to indicate hover state.
-  // d3.select(this).attr('stroke', 'black');
-  d3.select(this).attr("fill", d3.rgb(d.color).darker(0.5));
-
+  d3.select(this).attr('fill', d3.rgb(d.color).darker(0.5));
   const content = `<span class="value">tooltip is here</span><br/>`;
-
   tooltip.showTooltip(content, d3.event);
 }
 
-/*
-* Hides tooltip
-*/
 export function hideDetail(d) {
-  // debugger;
-  // reset outline
-  // d3.select(this).attr('stroke', d3.rgb(d.color).darker());
-  d3.select(this).attr("fill", d3.rgb(d.color));
+  d3.select(this).attr('fill', d3.rgb(d.color));
+  tooltip.hideTooltip();
+}
 
+export function showDetailLabelHover(d) {
+  const content = `<span class="value">tooltip is here</span><br/>`;
+  tooltip.showTooltip(content, d3.event);
+}
+
+export function hideDetailLabelHover(d) {
   tooltip.hideTooltip();
 }
